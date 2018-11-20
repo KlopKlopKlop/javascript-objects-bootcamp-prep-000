a8934501e19723b311e178475fdb27034330ea2b
Tim Flood <tflood9@gmail.com>
2018-11-20 14:52:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {
   Elvis: "Are you lonesome"
 };
 
-function updatePlaylist(obj, key, value){
-  return Object.assign({}, obj, { [key]: value})
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign({}, playlist, { [artistName]: songTitle})
 }
